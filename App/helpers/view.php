fc7799fcabd783fe60d4b0d4794f14f762418c6f
Abdelmoumen Mustafa <abdelmoumen.mustafa@gmail.com>
2023-10-04 00:20:45 +0100

    add status code to view helper

--- App/helpers/view.php
+++ App/helpers/view.php
@@ -1,15 +1,17 @@
 <?php
 
 // Load view helper
-function view($view, $data = [])
+function view($view, $data = [], $status = 200)
 {
     // check for view file
     if (file_exists('../App/views/' . $view . '.php')) {
+        http_response_code($status);
         extract($data);
 
         // require that view
         require_once "../App/views/" . $view . ".php";
     } else {
+        http_response_code(404);
         require_once "../App/views/errors/page_404.php";
     }
     exit;
