c14566c96369c9c2ae3528a1b4445ba3f4c35cf8
Abdelmoumen Mustafa <abdelmoumen.mustafa@gmail.com>
2023-10-07 15:40:07 +0100

    Fix Bug: request passed if the user didn't send csrf token

--- App/helpers/csrf_token.php
+++ App/helpers/csrf_token.php
@@ -1,17 +1,17 @@
 <?php
 
 function csrf_token($token = null) {
-    if($token){
-        // Check if the giving token is valid
-        if($token !== session('csrf_token')->get()){
-            return false;
-        }        
-        return true;
+    if($token === null){
+        // the string "lowhrs569874" appended to session_id because if the attacker knows the session id he can guess the csrf token by passing it to sha1() function
+        $token = sha1(session_id() . "lowhrs569874");
+
+        session('csrf_token')->set($token);
+        return $token;
     }
 
-    // the string "lowhrs569874" appended to session_id because if the attacker knows the session id he can guess the csrf token by passing it to sha1() function
-    $token = sha1(session_id() . "lowhrs569874");
-
-    session('csrf_token')->set($token);
-    return $token;
+    // Check if the giving token is valid
+    if($token !== session('csrf_token')->get()){
+        return false;
+    }        
+    return true;
 }
